30ef65c40b4a25474a80fc9d7da2ffdb1fe88cd0
Francesco Ferigo <ferigofra2000@gmail.com>
2018-04-25 21:29:34 +0000

    Pushed from Dropbox

--- execute.php
+++ execute.php
@@ -45,6 +45,9 @@ else
 {
 	$response = "Comando non valido!";
 }
+
+log_debug($text, 'logging $text');
+
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
