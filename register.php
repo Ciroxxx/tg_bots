c7601acd606e1e2b38574ccf2fbfde67d3a86044
Francesco Ferigo <ferigofra2000@gmail.com>
2018-05-06 07:33:39 +0000

    Pushed from Dropbox

--- register.php
+++ register.php
@@ -7,7 +7,7 @@ if(isset($_GET["bot_name"]) && array_key_exists($_GET["bot_name"], $bots)){
     $BOT_NAME = $_GET["bot_name"];
     $TOKEN = $bots[$bot_name];
     // PARAMETRI DA MODIFICARE
-    $WEBHOOK_URL = 'https://' . $BOT_NAME . '-killer-bot.herokuapp.com/execute.php;
+    $WEBHOOK_URL = 'https://' . $BOT_NAME . '-killer-bot.herokuapp.com/execute.php';
 
 
     // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
@@ -19,7 +19,7 @@ if(isset($_GET["bot_name"]) && array_key_exists($_GET["bot_name"], $bots)){
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
     curl_setopt($handle, CURLOPT_TIMEOUT, 60);
-    $result = curl_exec($handle);
+    //$result = curl_exec($handle);
     print_r($result);
     
 } else {
