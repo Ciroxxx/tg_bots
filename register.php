1f90c15859429be239d19cda9df22f00447c2374
Francesco Ferigo <ferigofra2000@gmail.com>
2018-05-06 07:20:54 +0000

    Pushed from Dropbox

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("required_common/bots.php");
+
+if(isset($_GET["bot_name"]) && array_key_exists($_GET["bot_name"], $bots)){
+    
+    $BOT_NAME = $_GET["bot_name"];
+    $TOKEN = $bots[$bot_name];
+    // PARAMETRI DA MODIFICARE
+    $WEBHOOK_URL = 'https://miner-killer-bot.herokuapp.com/execute.php?bot_name=' . $BOT_NAME;
+
+
+    // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+    $API_URL = 'https://api.telegram.org/bot' . $TOKEN .'/';
+    $method = 'setWebhook';
+    $parameters = array('url' => $WEBHOOK_URL);
+    $url = $API_URL . $method. '?' . http_build_query($parameters);
+    $handle = curl_init($url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+    $result = curl_exec($handle);
+    print_r($result);
+    
+} else {
+    exit;
+}
---
+++ required_common/bots.php
@@ -0,0 +1,10 @@
+<?php
+
+$bots = array(
+    "miner" => "572812206:AAERyKASDR4TIqwJiPnt17bNSE0kCwkazB8",
+    "brent" => "435363970:AAE96UAhhQCcpa3B6bnj36WGnBh63i2z0cM",
+    "captain" =>"559592888:AAEuEsT85IhpM0TTDHgD_Jvyy1aF1M8Eobg"
+);
+
+
+//to delete a webhook https://api.telegram.org/bot572812206:AAERyKASDR4TIqwJiPnt17bNSE0kCwkazB8/setwebhook LINK:https://stackoverflow.com/questions/32537081/how-to-use-getupdates-after-setwebhook-in-telegram-bot-api
