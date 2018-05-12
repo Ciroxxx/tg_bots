9367e59118e05ec272f88d85b0d6594773c1dbbf
Francesco Ferigo <ferigofra2000@gmail.com>
2018-05-12 07:17:51 +0000

    Pushed from Dropbox

--- required_common/Command.php
+++ required_common/Command.php
@@ -74,8 +74,10 @@ class Command{
 	function kill(){
 		//$buttons = array(array("adesso", "più tardi"), array("domani", "dopodomani"));
 		// $reply_markup = json_encode(array("keyboard" => $buttons, "resize_keyboard" => true,"one_time_keyboard" => true));
-		$buttons = array(array("text" => "testo1", "callback_data" => "dati callback"));
-		$reply_markup = json_encode(array("inline_keyboard" => $buttons));
+		//$buttons = array(array("text" => "testo1", "callback_data" => "dati callback"));
+		$inline_keyboard = array('inline_keyboard' => array(array('text' => 'Tasto1', 'callback_data' => 'pressed_btn1')));
+		//$reply_markup = json_encode(array("inline_keyboard" => $buttons));
+		$reply_markup = json_encode($inline_keyboard);
 		return $this -> send_text("bang! Sei morto stronzo/a", $reply_markup);
 	}
 
