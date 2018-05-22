c58840e7396a245807c6b0a921fa460b14a83ca4
Ciroxxx <ferigofra2000@gmail.com>
2018-05-23 00:37:46 +0200

    added testaccio to make some test

--- register.php
+++ register.php
@@ -3,12 +3,14 @@
 require_once("required_common/bots.php");
 
 if(isset($_GET["bot_name"]) && array_key_exists($_GET["bot_name"], $bots)){
-    
+
     $BOT_NAME = $_GET["bot_name"];
     $TOKEN = $bots[$BOT_NAME]["token"];
     // PARAMETRI DA MODIFICARE
     //$WEBHOOK_URL = 'https://' . $BOT_NAME . '-killer-bot.herokuapp.com/execute.php';
-    $WEBHOOK_URL = 'https://miner-killer-bot.herokuapp.com/execute.php?bot_name=' . $BOT_NAME;
+    //$WEBHOOK_URL = 'https://miner-killer-bot.herokuapp.com/execute.php?bot_name=' . $BOT_NAME;
+
+    $WEBHOOK_URL = 'https://miner-killer-bot.herokuapp.com/testaccio.php?bot_name=' . $BOT_NAME;
 
     // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
     $API_URL = 'https://api.telegram.org/bot' . $TOKEN .'/';
---
+++ testaccio.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'log_debug.php';
+
+log_debug($_GET, '$_GET');
