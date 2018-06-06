2b1534496a82c42c8333ff35652d1d3ab9e390f0
Ciroxxx <ferigofra2000@gmail.com>
2018-06-06 06:25:00 +0200

    test for result images

--- testaccio.php
+++ testaccio.php
@@ -3,7 +3,7 @@
 require_once 'log_debug.php';
 require_once 'functions.php';
 
-$results = google_images_search('pickard engage');
+$results = google_images_search('picard engage');
 
 foreach($results as $result){
   echo '<h3><img src="' . $result . '"></h3>';
