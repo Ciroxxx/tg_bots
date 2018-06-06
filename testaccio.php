48e79f70356cbec06d3f765eb27de36c0c2eb798
Ciroxxx <ferigofra2000@gmail.com>
2018-06-06 06:22:40 +0200

    test for result images

--- testaccio.php
+++ testaccio.php
@@ -1,5 +1,10 @@
 <?php
 
 require_once 'log_debug.php';
+require_once 'functions.php';
 
-log_debug($_GET, '$_GET');
+$results = google_images_search('pickard engage');
+
+foreach($results as $result){
+  echo '<h3><img src="' . $result . '"></h3>';
+}
