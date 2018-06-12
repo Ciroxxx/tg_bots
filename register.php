13569c5ec70fb741196295b082dda8a17a1727cc
Ciroxxx <ferigofra2000@gmail.com>
2018-06-12 06:36:02 +0200

    edited register.php

--- register.php
+++ register.php
@@ -23,7 +23,7 @@ if(isset($_GET["bot_name"]) && array_key_exists($_GET["bot_name"], $bots)){
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
     curl_setopt($handle, CURLOPT_TIMEOUT, 60);
-    $result = exec_curl_request($handle);
+    $result = curl_exec($handle);
     print_r($result);
 
 } else {
