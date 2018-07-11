9db33220eb994e368aa113e21864265a0b0745d3
Ciroxxx <ferigofra2000@gmail.com>
2018-07-12 00:29:38 +0200

    added callback mechanism

--- execute.php
+++ execute.php
@@ -102,18 +102,16 @@ if(isset($_GET["bot_name"]) && array_key_exists($_GET["bot_name"], $bots)){
 
         header("Content-Type: application/json");
 
-        $response = array("chat_id" => "92219874", "text" => array("ciao", "stronzo"), "method" => "sendMessage");
-
         echo json_encode($response);
 
-        // if($command -> callback !== false){
-        //     log_debug($command -> callback, 'if check true');
-        //     $callback_response = call_user_func_array(array($command, $command -> callback), array());
-        //     sleep(2);
-        //     log_debug($callback_response, '$callback_response');
-        //
-        //     echo json_encode($callback_response);
-        // }
+        if($command -> callback !== false){
+            // log_debug($command -> callback, 'if check true');
+            // $callback_response = call_user_func_array(array($command, $command -> callback), array());
+            // sleep(2);
+            // log_debug($callback_response, '$callback_response');
+            //
+            // echo json_encode($callback_response);
+        }
     }
 
 } else {
