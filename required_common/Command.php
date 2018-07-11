f3ae36e2fa813c91a2368550c6de894ac0a37649
Ciroxxx <ferigofra2000@gmail.com>
2018-07-11 23:24:39 +0200

    added callback mechanism

--- required_common/Command.php
+++ required_common/Command.php
@@ -250,7 +250,7 @@ class Command{
 		$caption = "Chi osa conferire con\n\n" . $araldica;
 		$url = get_protocol() . '://' . $_SERVER['HTTP_HOST'] . '/' . 'images/araldica.jpg';
 
-		$this -> callback = "araldicgif";
+		$this -> callback = "mine";
 		return $this -> send_photo($url, '', $caption);
 	}
 
